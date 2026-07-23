b7c4a93600678f92b38abc40f1a89180e5685467
Jeremias Leão <jereleao@hotmail.com>
2026-07-23 09:40:08 -0300

    change to warn

--- src/app/(modules)/account/page.tsx
+++ src/app/(modules)/account/page.tsx
@@ -7,7 +7,7 @@ import { api } from "~/trpc/server";
 export default async function AccountPage() {
   const userData = await api.user.me();
 
-  console.log("userData: ", userData);
+  console.warn("userData: ", userData);
 
   if (!userData) redirect("/login");
 
