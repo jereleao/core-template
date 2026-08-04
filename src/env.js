09338e51394823b3a4ed0d6fed6c919854b2398e
Jeremias Leão <jereleao@hotmail.com>
2026-08-04 12:38:23 -0300

    add DEFAULT_LANGUAGE

--- src/env.js
+++ src/env.js
@@ -27,6 +27,7 @@ export const env = createEnv({
     EMAIL_SERVER_USER: z.string(),
     EMAIL_SERVER_PASSWORD: z.string(),
     EMAIL_FROM: z.string().email(),
+    DEFAULT_LANGUAGE: z.string().default("en")
   },
       
   /**
@@ -59,6 +60,7 @@ export const env = createEnv({
     EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
     EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
     EMAIL_FROM: process.env.EMAIL_FROM,
+    DEFAULT_LANGUAGE: process.env.DEFAULT_LANGUAGE,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
--- src/libs/i18n/request.ts
+++ src/libs/i18n/request.ts
@@ -1,9 +1,11 @@
 import { cookies } from "next/headers";
 import { getRequestConfig } from "next-intl/server";
+import { env } from "~/env";
 
 export default getRequestConfig(async (params) => {
   const store = await cookies();
-  const locale = params.locale || store.get("locale")?.value || "en";
+  const locale =
+    params.locale || store.get("locale")?.value || env.DEFAULT_LANGUAGE;
   const messages = (await import(`../../../messages/${locale}.json`)).default;
 
   return {
